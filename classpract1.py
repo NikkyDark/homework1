4cf421c8cda1bef71f715cd856927f13298cf016
Nikita Dark <bobachev_nik@mail.ru>
2024-12-27 23:07:29 +0300

    Update classpract1.py

--- classpract1.py
+++ classpract1.py
@@ -22,14 +22,14 @@ if __name__ == '__main__':
     while True:
         choice = input("Приветствую! Выберите действие: \n1 - Вход\n2 - Регистрация\n")
         if choice == 1:
-            login = input("Вdедите логин ")
+            login = input("Введите логин ")
             password = input("Введите пароль ")
             if login in database.data:
                 print("ok")
             else:
                 print("error")
         if choice == 2:
-            user = User(input('Ввеlите логин: '), password := input('Введите пароль: '),
+            user = User(input('Введите логин: '), password := input('Введите пароль: '),
                         password2 := input('Повторите пароь: '))
             if password != password2:
                 exit()
